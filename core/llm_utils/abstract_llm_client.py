e3172be74926d558b910a2f06baf806df2c445da
Juncheng Dong <ge85dih@mytum.de>
2024-06-02 18:34:19 +0200

    Refine LLM client

--- core/llm_utils/abstract_llm_client.py
+++ core/llm_utils/abstract_llm_client.py
@@ -2,4 +2,7 @@ from abc import ABC
 
 
 class AbstractLLMClient(ABC):
-    pass
+
+    def process_paper_on_tags(self, file_obj, tags, *args, **kwargs):
+        raise NotImplementedError(f"Abstract method {self.__class__.__name__} not implemented!")
+
