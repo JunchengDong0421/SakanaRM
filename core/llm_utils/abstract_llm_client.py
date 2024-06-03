6df937f2cfbb1de19654b72db4907f5bcecee5fe
Juncheng Dong <ge85dih@mytum.de>
2024-06-03 10:32:38 +0200

    Add abstractmethod decorator

--- core/llm_utils/abstract_llm_client.py
+++ core/llm_utils/abstract_llm_client.py
@@ -1,8 +1,9 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class AbstractLLMClient(ABC):
 
+    @abstractmethod
     def process_paper_on_tags(self, *args, **kwargs):
         raise NotImplementedError(f"Abstract method {self.__class__.__name__} not implemented!")
 
