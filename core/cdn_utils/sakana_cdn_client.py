f91823d735cfbe0aa1784f1f71eb74e88a826892
Juncheng Dong <ge85dih@mytum.de>
2024-06-25 08:09:58 +0200

    Better error wrapping and handling

--- core/cdn_utils/sakana_cdn_client.py
+++ core/cdn_utils/sakana_cdn_client.py
@@ -18,45 +18,57 @@ class SakanaCDNClient(AbstractCDNClient):
         return cls.__instance
 
     def store_paper(self, fd):
-        with fd:
-            filename = random_filename()
-            url = f"{self.config.CDN_HOST}{self.config.RESOURCE_REQUEST_PATH}{filename}"
-            files = {"file": (filename, fd)}
-            res = requests.post(url, files=files)
-        json_res = res.json()
-        if res.status_code != 201 or 'filepath' not in json_res:
-            # raise error and catch in core/views.py
-            raise CDNClientException(json_res.get("error", "Something went wrong"))
+        try:
+            with fd:
+                filename = random_filename()
+                url = f"{self.config.CDN_HOST}{self.config.RESOURCE_REQUEST_PATH}{filename}"
+                files = {"file": (filename, fd)}
+                res = requests.post(url, files=files)
+            json_res = res.json()
+            if res.status_code != 201 or 'filepath' not in json_res:
+                raise ValueError(json_res.get("error", "Something went wrong"))
 
-        filepath = json_res['filepath']
-        return filepath
+            filepath = json_res['filepath']
+            return filepath
+        except Exception as e:
+            # wrap, raise error and catch in core/views.py
+            raise CDNClientException(str(e))
 
     def request_for_paper(self, filepath):
-        res = requests.get(filepath)
-        if res.status_code != 200:
-            json_res = res.json()
-            raise CDNClientException(json_res.get("error", "Something went wrong"))
+        try:
+            res = requests.get(filepath)
+            if res.status_code != 200:
+                json_res = res.json()
+                raise ValueError(json_res.get("error", "Something went wrong"))
 
-        fd = BytesIO(res.content)
-        return fd
+            fd = BytesIO(res.content)
+            return fd
+        except Exception as e:
+            raise CDNClientException(str(e))
 
     def delete_paper(self, filepath):
-        res = requests.delete(filepath)
-        json_res = res.json()
-        if res.status_code != 200 or 'filepath' not in json_res:
-            raise CDNClientException(json_res.get("error", "Something went wrong"))
+        try:
+            res = requests.delete(filepath)
+            json_res = res.json()
+            if res.status_code != 200 or 'filepath' not in json_res:
+                raise ValueError(json_res.get("error", "Something went wrong"))
 
-        filepath = json_res['filepath']
-        return filepath
+            filepath = json_res['filepath']
+            return filepath
+        except Exception as e:
+            raise CDNClientException(str(e))
 
     def replace_paper(self, filepath, new_fd):
-        with new_fd:
-            filename = random_filename()
-            files = {"file": (filename, new_fd)}
-            res = requests.put(filepath, files=files)
-        json_res = res.json()
-        if res.status_code != 200 or 'filepath' not in json_res:
-            raise CDNClientException(json_res.get("error", "Something went wrong"))
+        try:
+            with new_fd:
+                filename = random_filename()
+                files = {"file": (filename, new_fd)}
+                res = requests.put(filepath, files=files)
+            json_res = res.json()
+            if res.status_code != 200 or 'filepath' not in json_res:
+                raise ValueError(json_res.get("error", "Something went wrong"))
 
-        new_filepath = json_res['filepath']
-        return new_filepath
+            new_filepath = json_res['filepath']
+            return new_filepath
+        except Exception as e:
+            raise CDNClientException(str(e))
