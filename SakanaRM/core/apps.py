7901e26249a96c8e3de31ed644e4cb91ca7b7403
Juncheng Dong <ge85dih@mytum.de>
2024-06-25 20:17:22 +0200

    Revert adding cleanup code in apps.py

--- SakanaRM/core/apps.py
+++ SakanaRM/core/apps.py
@@ -1,20 +1,6 @@
-import json
-
 from django.apps import AppConfig
-from django.db.models import Q
 
 
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
-
-    def ready(self):
-        # Must import models inside function, otherwise AppRegistryNotReady exception
-        from .models import Paper, Workflow
-        # Set status of all pending workflows to failed
-        _ = Workflow.objects.filter(status=Workflow.StatusChoices.PENDING) \
-            .update(status=Workflow.StatusChoices.FAILED,
-                    result=json.dumps({"error": "Server restart, workflow progress is lost"})
-                    )
-        # Delete all papers with an empty filepath
-        _, _ = Paper.objects.filter(Q(file_path__isnull=True) | Q(file_path="")).delete()
