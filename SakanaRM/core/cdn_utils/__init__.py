ef1082a8f9f84106c7018124c05f85e140bce723
Juncheng Dong <ge85dih@mytum.de>
2024-06-26 16:20:12 +0200

    Remove unused files

--- SakanaRM/core/cdn_utils/__init__.py
+++ SakanaRM/core/cdn_utils/__init__.py
@@ -1,2 +1,1 @@
-from .pseudo_cdn_client import PseudoCDNClient
 from .sakana_cdn_client import SakanaCDNClient
--- SakanaRM/core/llm_utils/__init__.py
+++ SakanaRM/core/llm_utils/__init__.py
@@ -1,3 +1,2 @@
 from .gpt_client import GPTClient
-from .pseudo_llm_client import PseudoLLMClient
 from .simple_keyword_client import SimpleKeywordClient
